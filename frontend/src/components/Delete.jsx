e6e1a5b346a3f4324879f66e6ff95982d6187435
Kabuddy768 <benmwangi968@gmail.com>
2026-09-18 17:05:17 +0300

    Initial commit

---
+++ frontend/src/components/Delete.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Delete = () =>{
+    return(
+        <div>This is the Deletepage</div>
+    )
+}
+
+export default Delete
---
+++ frontend/src/components/Edit.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Edit = () =>{
+    return(
+        <div>This is the Editpage</div>
+    )
+}
+
+export default Edit 
